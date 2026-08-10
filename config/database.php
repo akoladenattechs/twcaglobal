a3b77513d53a9da372d23951554c3bb0e62ac42f
akoladenattechs <akolade@nattechs.ng>
2026-08-10 22:31:41 +0100

    Fix: set default mysql host fallback to localhost in config/database.php

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT'),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
